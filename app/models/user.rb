4857f317ac75de017517c43db9c9d06142595948
vzdrizhni <vzdrizhni@gmail.com>
2020-06-07 00:19:32 +0500

    added feature test for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
                      size: { less_than: 5.megabytes,
                              message: 'should be less than 5MB' },
                      presence: true
-  validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :name, presence: true, length: { in: 3..50 }, uniqueness: true
 
   before_save { self.name = name.capitalize }
 end
