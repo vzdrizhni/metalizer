66b0d90e6038f66c34c2eea6d444ee366a79fa12
vzdrizhni <vzdrizhni@gmail.com>
2020-06-08 01:18:09 +0500

    fixing heroku static files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.serve_static_assets = true
 end
