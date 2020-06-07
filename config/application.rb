25cc5257cf5281553dca3e7c49c94c088bb955ae
vzdrizhni <vzdrizhni@gmail.com>
2020-06-08 02:25:19 +0500

    another try

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Metalizer
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join(app/assets/fonts)
   end
 end
